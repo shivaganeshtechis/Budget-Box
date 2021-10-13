d681e0594ca7a8d8f727207b4d73a4570f993f74
Ngin Hak <n-hak@beniten.com>
2021-10-13 17:36:04 +0700

    add layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { LayoutProvider } from 'react-page-layout';
 import DefaultLayout from './components/default/DefaultLayout';
+import PublicDefaultLayout from './components/default/PublicDefaultLayout';
 
 const layouts = {
-	'public': DefaultLayout,
+	'public': PublicDefaultLayout,
+	'default': DefaultLayout
 };
 
 function App() {
