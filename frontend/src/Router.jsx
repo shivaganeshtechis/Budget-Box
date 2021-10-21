df5c2470c821ca3ecb3f9ac89a02d6561240511f
l-panhavoan <l-sovanpanhavoan@beniten.com>
2021-10-21 10:09:51 +0700

    landing page responsive and add sign up page

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -8,6 +8,7 @@ import { fetchUserFromLocalStorage } from "./reducks/users/operations";
 import { getUser } from "./reducks/users/selectors";
 import Transaction from "./containers/Transaction";
 import MyProfile from "./containers/MyProfile";
+import SignUp from "./containers/Signup";
 
 const Router = () => {
     const dispatch = useDispatch();
@@ -24,6 +25,7 @@ const Router = () => {
             <Switch>
                 <Route exact path={"/"} component={token ? Report : Home} />
                 <Route exact path={"/sign-in"} component={token ? Report : SignIn} />
+                <Route exact path={"/sign-up"} component={token ? Report : SignUp} />
                 <Route exact path={"/transaction"} component={token ? Transaction : SignIn} />
                 <Route exact path={"/profile"} component={token ? MyProfile : SignIn} />
             </Switch>
